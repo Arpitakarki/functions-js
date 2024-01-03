aeaf9dfd8c8e16a1a001ee77b633ace410ff8fbe
Arpita karki <151529261+Arpitakarki@users.noreply.github.com>
2024-01-03 07:04:31 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function myName(me,him){
+ return me * him
+}
+
+let ak=myName(28, 45);
+
+document.getElementById("demo").innerHTML = "1)" + " " + ak;
+
+function naMe(her){
+    return (5/9) * (her-32)
+}
+   
+let bk=naMe(45);
+   
+document.getElementById("demo2").innerHTML = "2)" + " " + bk;
